927dc95fc386ea1755bb295f0109629f9fa2941c
hmathir <hmathir@gmail.com>
2022-08-17 21:28:44 +0600

    Add Validation on Deposit & Withdraw

--- withdrow.js
+++ withdrow.js
@@ -1,16 +1,18 @@
 
 document.getElementById('Withdrow-button').addEventListener('click', function(){
-
-
     const newWithdrowAmount = getInputValueById('input-Withdrow');
+    if(isNaN(newWithdrowAmount)){
+        alert('Error');
+        return;
+    }
     const newWithdrowElement= getTextElementById('Withdrow');
-
     const newWithdrowTotal = newWithdrowElement + newWithdrowAmount;
-    setTextElementValueById('Withdrow', newWithdrowTotal );
-
     const previousBlanceTotal = getTextElementById('Blance');
-
+    if(newWithdrowAmount > previousBlanceTotal || newWithdrowAmount < 0){
+        alert('Fund Shortage');
+        return false;
+    }
+    setTextElementValueById('Withdrow', newWithdrowTotal );
     const newBlanceTotal = previousBlanceTotal - newWithdrowAmount;
     setTextElementValueById('Blance', newBlanceTotal);
-
 })
